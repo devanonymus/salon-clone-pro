8010eb86daca1d9c4c6ce8e13fef0be936784eb5
FormaLab <formalab@Mac.home>
2026-05-04 09:09:07 +0200

    Import inventory module in sales

--- apps/api/src/sales/sales.module.ts
+++ apps/api/src/sales/sales.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { SalesController } from './sales.controller';
-import { SalesService } from './sales.service';
-import { PrismaService } from '../prisma.service';
+import { Module } from "@nestjs/common";
+import { SalesController } from "./sales.controller";
+import { SalesService } from "./sales.service";
+import { PrismaService } from "../prisma.service";
+import { InventoryModule } from "../inventory/inventory.module";
 
 @Module({
+  imports: [InventoryModule],
   controllers: [SalesController],
   providers: [SalesService, PrismaService],
 })
